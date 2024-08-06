d6d31ce638e96d2850c1bb83752e56c2407f3a88
Ritwick Manatkar <ritwickmanatkar@gmail.com>
2024-08-06 12:49:09 -0700

    Skeleton of the backend created.
    Setup of Node and Express App utilizing Typescript is done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,20 +2,12 @@ import express from 'express';
 
 const app = express();
 
-// Standard Routes for GET/POST/PUT/DELETE
+// Middlewares
 app.use(express.json());
-app.get("/hello", (request, response, next) => {
-  console.log(request.body.name);
-  return response.send("Hello from the GET middleware...");
-});
 
-// Dynamic Routes for GET/POST/PUT/DELETE
-app.use(express.json());
-app.get("/user/:id", (request, response, next) => {
-  console.log(request.body.name);
-  console.log(request.params.id);
-  return response.send("Hello from the GET middleware...");
-});
+
+
+// Connections and Listeners
 
 // For my Macbook port 5000 was in use due to Airplay settings being turned on. 
 // I turned it off to make this work.
