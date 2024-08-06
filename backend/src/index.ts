7dbf255489f734315fa54276275736f2cd92913e
Ritwick Manatkar <ritwickmanatkar@gmail.com>
2024-08-06 12:44:52 -0700

    learning about middlewares and connection in nodejs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,22 @@
-console.log(
-    "Hello World."
-  );
+import express from 'express';
+
+const app = express();
+
+// Standard Routes for GET/POST/PUT/DELETE
+app.use(express.json());
+app.get("/hello", (request, response, next) => {
+  console.log(request.body.name);
+  return response.send("Hello from the GET middleware...");
+});
+
+// Dynamic Routes for GET/POST/PUT/DELETE
+app.use(express.json());
+app.get("/user/:id", (request, response, next) => {
+  console.log(request.body.name);
+  console.log(request.params.id);
+  return response.send("Hello from the GET middleware...");
+});
+
+// For my Macbook port 5000 was in use due to Airplay settings being turned on. 
+// I turned it off to make this work.
+app.listen(5000, () => console.log("Server is now open..."));
