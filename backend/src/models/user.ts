10b5648a87f64dada1c1560b44b56f1c8c307cdf
Ritwick Manatkar <ritwickmanatkar@gmail.com>
2024-08-06 15:24:39 -0700

    Schema for Users and chats generated

---
+++ backend/src/models/user.ts
@@ -0,0 +1,43 @@
+// Imports
+import mongoose from "mongoose";
+import { randomUUID } from 'crypto';
+
+// Schema for Chats
+const chatSchema = new mongoose.Schema({
+    // Random UUID for Chat ID
+    id: {
+        type: String,
+        default: randomUUID()
+    },
+    // 2 roles: AI chatbot or User
+    role: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    }
+});
+
+// Schema for Users
+const userSchema = new mongoose.Schema({
+    // ID Provided automatically
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    chats: [chatSchema]
+});
+
+// Export the Schema
+export default mongoose.model('User', userSchema);
