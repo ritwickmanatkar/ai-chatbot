8a1cff9c1ceeb65e88b60c165e6900a0827c586d
Ritwick Manatkar <ritwickmanatkar@gmail.com>
2024-08-07 15:50:41 -0700

    Skeleton frontend routes created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,26 @@
 // Imports
+import { Routes, Route } from 'react-router-dom';
+
+import Header from "./components/Header"
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Chat from './pages/Chat';
+import NotFound from './pages/NotFound';
 
 // Application
 function App() {
   return (
-    <main>Hello World...</main>
+    <main>
+      <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/chat' element={<Chat />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+    </main>
   )
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createTheme, ThemeProvider } from '@mui/material'
+import { BrowserRouter } from 'react-router-dom'; 
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// Theme for MaterialUI
+const theme = createTheme({
+  typography: {
+    fontFamily: "Roboto Slab, serif", 
+    allVariants: {color: 'white'}
+  }
+});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
