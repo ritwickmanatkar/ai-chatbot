a52925b978a6bd569bb87be4c5fb3f00e9b14bc0
Ritwick Manatkar <ritwickmanatkar@gmail.com>
2024-08-07 17:04:39 -0700

    Added the app logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { AppBar, Toolbar } from '@mui/material';
+import Logo from './shared/Logo';
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+  return <AppBar sx={{bgcolor: "transparent", position: 'static', boxShadow: 'none'}}>
+    <Toolbar sx={{display: 'flex'}}>
+        <Logo />
+    </Toolbar>
+  </AppBar>
 }
 
 export default Header;
