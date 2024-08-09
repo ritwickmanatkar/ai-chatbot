6063926c3fafa23ebb22f7806c5cbb31cd3b7232
Ritwick Manatkar <ritwickmanatkar@gmail.com>
2024-08-09 15:14:43 -0700

    Whitelisted FE to remove cors error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,12 +4,14 @@ import { config } from 'dotenv';
 import morgan from 'morgan';
 import appRouter from './routes/index.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 // --------- Declarations --------- 
 const app = express();
 config();
 
 // --------- Middlewares --------- 
+app.use(cors({origin: "http://localhost:5173", credentials:true})); 
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
