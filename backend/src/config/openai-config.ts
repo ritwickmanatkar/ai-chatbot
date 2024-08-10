5ab49b0b8d6e2d521882494c8104c9b1e22a50d8
Ritwick Manatkar <ritwickmanatkar@gmail.com>
2024-08-09 19:14:44 -0700

    Bugfix in configurations

--- backend/src/config/openai-config.ts
+++ backend/src/config/openai-config.ts
@@ -2,9 +2,10 @@
 import { Configuration } from "openai";
 
 // Configuration function
-export function configureOpenAI() {
+export const configureOpenAI = () => {
     const config = new Configuration({
         apiKey: process.env.OPEN_AI_SECRET,
         organization: process.env.OPEN_AI_ORGANIZATION_ID
     });
+    return config;
 };
